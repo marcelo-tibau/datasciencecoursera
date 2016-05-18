bcd5d15a12a73e2bbda5eff4cdd6567b04d3bd2e
Marcelo Tibau <marcelo@tibau.org>
2016-05-18 17:23:22 -0300

    Add files via upload

--- best.R
+++ best.R
@@ -1,25 +1,30 @@
-best <- function(state, outcome){
-  path = directory
-  directory <- ("rprog-data-ProgAssignment3-data")
-  fileName <- ("outcome-of-care-measures.csv")
-  data <- read.csv(file.path(directory, fileName))
-  State = data[, "State"]
-  Hospital = data[, "Hospital.Name"]
-  Heart_attack = data[, "11"]
-  Heart_failure = data[, 17]
-  Pneumonia = data[, 23]
-  my_data(data.frame(Heart_attack, Heart_failure, Pneumonia, State, Hospital.Name))
-  bool.state = my_data[, "State"] == state
-  state.data = my_data[bool.state, ]
-  state.data = na.omit(state.data)
+best <- function(state, outcomeName){
+  # Read the oucome data
+  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  states <- outcome$State
+  diseases <- c("heart attack","heart failure","pneumonia")
   
-  if(nrow(state.data) == 0){
-    stop("Invalid state")
+  # Check the validity of state and outcome
+  if (! state %in% states ){
+    stop("invalid state")
   }
-  
-  if(outcome == "Heart_attack"){
-    minRate = min(state.data[,1])
-    minHospital = state.data[which(state.data[,1] == minRate), Hospital.Name]
-    
+  if (! outcomeName %in% diseases){
+    stop("invalid outcome")
   }
+  # Return the hospital name in one particular state with the lowest 30-day death rate
+  col_id <- 0
+  if (outcomeName == diseases[1]){
+    col_id <- 11
+  } else if (outcomeName == diseases[2]) {
+    col_id <- 17
+  } else if (outcomeName == diseases[3]) {
+    col_id <- 23
+  }
+  outcome.sub <- outcome[outcome[,7] == state, ]
+  outcome.sub2 <- outcome.sub[,c(2,7,col_id)]
+  outcome.sub3 <- outcome.sub2[outcome.sub2[,3]!="Not Available",]
+  outcome.sub3[,3] <- as.numeric(outcome.sub3[,3])
+  outcome2 <- outcome.sub3
+  bestrow <- outcome2[outcome2[,3] == min(outcome2[,3]),]
+  as.character(bestrow[1])
 }
