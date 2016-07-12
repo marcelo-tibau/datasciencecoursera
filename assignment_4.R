ba15fed1e6c6a8b7287ab3f7299a01e94b30e0bf
Marcelo Tibau <marcelo@tibau.org>
2016-07-12 14:14:56 -0300

    Add files via upload

---
+++ assignment_4.R
@@ -0,0 +1,55 @@
+## To read the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+# Code to sum the total emissions per year, and then plot using the barplot in order to show the comparison.
+Plot_emissions_year <- tapply(NEI$Emissions, NEI$year, FUN = sum)
+barplot(Plot_emissions_year, xlab = "Year", ylab = "Emissions Rate", title(main = "Comparing PM25 emissions per year"))
+dev.copy(png, file="pm_plot1.png", height=480, width=480)
+dev.off()
+
+## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008?
+# Code to subset the data to Baltimore.
+Subset_Baltimore <- NEI[NEI$fips=="24510", ]
+# Code to sum the total emissions per year and plot the comparison.
+Plot_Baltimore <- aggregate(Emissions ~ year, Subset_Baltimore, sum)
+barplot(height = Plot_Baltimore$Emissions, names.arg = Plot_Baltimore$year, xlab = "Year", ylab = "Emissions Rate", title(main = "Comparing PM25 emissions per year in Baltimore"))
+dev.copy(png, file="pm_plot2.png", height=480, width=480)
+dev.off()
+  
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+## Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999-2008 for Baltimore City? Which have seen increases in emissions from 1999-2008?
+# Code to load the package ggplot2.
+library(ggplot2)
+# Code to subset the data to Baltimore.
+Subset_Baltimore_point <- NEI[NEI$fips=="24510", ]
+# Code to subset again, this time per type.
+Plot_type <- aggregate(Emissions ~ year + type, Subset_Baltimore_point, sum)
+# Code to plot the four types of sources.
+p <- ggplot(Plot_type, aes(year, Emissions, color = type))
+p <- p + geom_line() + xlab("Year") + ylab("Emissions Rate") + ggtitle("Comparing PM25 emissions in Baltimore per type")
+print(p)
+dev.copy(png, file="pm_plot3.png", height=480, width=640)
+dev.off()
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+# Code to load the package ggplot2.
+library(ggplot2)
+## Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+# Code to merge the datasets.
+Merged_datas <- merge(NEI, SCC, by="SCC")
+# Code to subset the data to emissions from coal combustion-related.
+Subset_Coal <- grepl("coal", Merged_datas$Short.Name, ignore.case = TRUE)
+Subset_Coal_Used <- Merged_datas[Subset_Coal, ]
+#Code to sum the emissions per year and plot the comparison.
+Plot_Coal <- aggregate(Emissions ~ year, Subset_Coal_Used, sum)
+p1 <- ggplot(Plot_Coal, aes(factor(year), Emissions))
+p1 <- p1 + geom_bar(stat = "identity") + xlab("Year") + ylab("Emissions Rate") + ggtitle("Comparing Emissions from Coal")
+print(p1)
+dev.copy(png, file="pm_plot4.png", height=480, width=640)
+dev.off()
+
