56daed41fa93a3343a7399c897a0ada4c1ca1c25
Marcelo Tibau <marcelo@tibau.org>
2016-07-27 17:02:54 -0300

    Add files via upload

--- Code_RepData_PeerAssessment1.R
+++ Code_RepData_PeerAssessment1.R
@@ -75,117 +75,21 @@ print(total_ac_steps_median)
 
 ### Are there differences in activity patterns between weekdays and weekends?
 ## Code to create the new factor variable in the dataset with the levels "weekday" and "weekend".
-new_activity$weekday <- weekdays(as.Date(new_activity$date, format = "%Y-%m-%d"))
-#new_activity$weekday.type = factor(ifelse(new_activity$weekday == "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" | new_activity$weekday == "Saturday", "Sunday", levels = c("weekday", "weekend")))
-new_activity$weekday.type = factor(
-        if (weekday == "Saturday")
-                {weekday.type == "weekend"}
-        if (weekday == "Sunday")
-                {weekday.type == "weekend"}
-        else {weekday.type == "weekday"}
-        )
+week_activity <- new_activity
 
-## Code to calculate the average number of steps per interval
-new_activity_weekdays = new_activity[new_activity$weekday.type == "weekday", ]
-new_activity_weekend = new_activity[new_activity$weekday.type == "weekend", ]
-avg_weekdays = sapply(split(new_activity_weekdays$steps, new_activity_weekdays$interval), mean)
-avg_weekend = sapply(split(new_activity_weekend$steps, new_activity_weekend$interval), mean)
+week_activity$date <- as.Date(strptime(week_activity$date, format="%Y-%m-%d")) 
 
-ifelse()
+week_activity$day <-  factor(ifelse(as.POSIXlt(week_activity$date)$wday %in% c(0,6), 'weekend', 'weekday'))
 
+## Code to calculate the averaged number of steps taken by weekday or weekend.
+averaged_week_activity <- aggregate(steps ~ interval + day, data=week_activity, mean)
 
+## Code to plot a time series of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all weekday days or weekend days (y-axis).
 
+library(ggplot2)
+ggplot(averaged_week_activity, aes(interval, steps)) + 
+        geom_line() + 
+        facet_grid(day ~ .) +
+        xlab("5-minute interval") + 
+        ylab("avarage number of steps")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-week_activity <- function(date) {
-        day <- weekdays(date)
-        if (day %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
-                return("weekday")
-        else if (day %in% c("Saturday", "Sunday"))
-                return("weekend")
-        else
-                stop("incorrect")
-}
-new_activity$date <- as.Date(new_activity$date)
-new_activity$day <- sapply(new_activity$date, FUN = week_activity)
-
-##
-weekdays_steps <- function(data) {
-        weekdays_steps <- aggregate(data$steps, by=list(interval = data$interval),
-                                    FUN=mean, na.rm=T)
-        weekdays_steps$interval <- 
-                as.integer(levels(weekdays_steps$interval)[weekdays_steps$interval])
-        colnames(weekdays_steps) <- c("interval", "steps")
-        weekdays_steps
-}
-
-data_by_weekdays <- function(data) {
-        data$weekday <- 
-                as.factor(weekdays(data$date)) 
-        weekend_data <- subset(data, weekday %in% c("Saturday","Sunday"))
-        weekday_data <- subset(data, !weekday %in% c("Saturday","Sunday"))
-        
-        weekend_steps <- weekdays_steps(weekend_data)
-        weekday_steps <- weekdays_steps(weekday_data)
-        
-        weekend_steps$dayofweek <- rep("weekend", nrow(weekend_steps))
-        weekday_steps$dayofweek <- rep("weekday", nrow(weekday_steps))
-        
-        data_by_weekdays <- rbind(weekend_steps, weekday_steps)
-        data_by_weekdays$dayofweek <- as.factor(data_by_weekdays$dayofweek)
-        data_by_weekdays
-}
-
-data_weekdays <- aggregate(data_by_weekdays(new_activity))
-
-
-
-## Code to make the time series plot of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all weekday days or weekend days (y-axis).
-library(lattice)
-xyplot(new_activity$weekday ~ interval | steps,
-       geom_line(),
-       layout = c(1,2),
-       main = "Time series plot of steps by interval - weekday and weekend views",
-       xlab = "Interval",
-       ylab = "Number of steps")
-
-## plot time series
-library(lattice)
-xyplot(
-        mean_steps ~ interval | daytype,
-        newinterval,
-        type = "l",
-        layout = c(1,2),
-        main = "Time Series Plot of the 5-Minute Interval\nand the Average Number of Steps Taken,\nAveraged Across All Weekday Days or Weekend Days",
-        xlab = "5-Minute Interval",
-        ylab = "Average Number of Steps Taken"
-)
